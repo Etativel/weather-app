f9b8e76e4b5ceaa35b8809385f88683f45a1aac8
Etativel <farhanmaulana4611@gmail.com>
2024-09-28 14:24:39 +0700

    styling

--- src/js/index.js
+++ src/js/index.js
@@ -20,3 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const profileImgContainer = document.querySelector(".profile-img");
 profileImgContainer.src = profileImg;
+
+import currIcon from "../assets/icons/5729381_snowflake_weather_winter_snowing_snow_forecast.png";
+
+const currentWeatherIcons = document.querySelector(".curr-weather-icon");
+
+currentWeatherIcons.src = currIcon;
+
+import todayI from "../assets/icons/5729382_forecast_sun_cloud_weather_raining.png";
+
+const todayIcon = document.querySelectorAll(".today-icn");
+
+todayIcon.forEach((icon) => {
+  icon.src = todayI;
+});
+
+import todaybg from "../assets/cloudy-sky-landscape-wallpaper.jpg";
+
+const todayback = document.querySelectorAll(".today-ctr");
+todayback.forEach((container) => {
+  // container.style.backgroundImage = `url(${todaybg})`;
+});
